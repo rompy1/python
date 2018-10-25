916aa4ef93cf5090e2a9de263b04aec730a20628
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-10-25 11:10:31 -0700

    Update guessnumber2.py

--- guessnumber2.py
+++ guessnumber2.py
@@ -3,9 +3,9 @@ exit = 0
 while exit == 0:
   print("Welcome to the number guessing game!")
   print("The object of the game is to guess my number.")
-  print("The number will be between 1 and 50.")
-  guess = 0
-  number = random.randint(1,50)
+  print("The number will be between 1 and 100.")
+  guess = -5
+  number = random.randint(0,100)
   tries = 0
   endgame = 0
   while endgame == 0:
@@ -16,11 +16,10 @@ while exit == 0:
     elif guess < number:
       print("My number is bigger than that!")
     else:
-      print("You guessed my number in %d attempts!" % tries)
+      print(f"You guessed my number in {tries} attempts!")
       endgame = 1
     if tries >= 10:
       print("I'm sorry, you ran out of tries! Game Over.")
-      endgame = 1
   input("Press enter to play again.")
   print("\n")
   
